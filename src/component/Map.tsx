66dfc5b6ba1e51776eb36df74b9b1596707bdb55
christophe-chausseray <chausseray.christophe@gmail.com>
2020-12-16 19:22:46 +0100

    Add tooltip with name on marker

--- src/component/Map.tsx
+++ src/component/Map.tsx
@@ -1,7 +1,9 @@
 
-import React from 'react';
-import GoogleMapReact from 'google-map-react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import GoogleMapReact from 'google-map-react';
+import { Icon } from '@iconify/react';
+import mapMarker from '@iconify/icons-mdi/map-marker';
 import { useListCampings } from './../hook';
 import { Camping } from '../model';
 
@@ -11,22 +13,52 @@ const Container = styled.div`
   position: relative;
 `;
 
-function CampingMarker({lng, lat, name}: { lng: number, lat: number, name: string }): JSX.Element {
-  const size = 10;
-  const campingMarkerStyle = {
-    position: 'absolute',
-    width: size,
-    height: size,
-    left: -size / 2,
-    top: -size / 2,
+const Marker = styled.div`
+  display: block;
+`;
 
-    border: '5px solid #00b92a',
-    borderRadius: size,
-    backgroundColor: '#00b92a'
-  };
+const InfoWindow = styled.div`
+  position: absolute;
+  bottom: 5px;
+  left: -20px;
+  width: 80px;
+  z-index: 1000000;
+  background-color: #3986ac;
+  padding: 10px;
+  border-radius: 5px;
+  box-shadow: 2px 2px 12px rgba(0,0,0,.5);
+  color: #ffff;
+  font-family: ui-sans-serif;
+  &:after {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -15px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: #3986ac transparent transparent transparent;
+  }
+`;
+
+function CampingMarker({name}: { name: string }): JSX.Element {
+  const [infoWindowIsOpen, setInfoWindowIsOpen] = useState(false);
 
   return (
-    <div style={campingMarkerStyle} />
+    <Marker>
+      {infoWindowIsOpen &&
+        <InfoWindow>
+          {name}
+        </InfoWindow>
+      }
+      <Icon
+        icon={mapMarker}
+        width={40}
+        color="#2f7510"
+        onMouseEnter={() => setInfoWindowIsOpen(true)}
+        onMouseLeave={() => setInfoWindowIsOpen(false)}
+      />
+    </Marker>
   );
 }
 
