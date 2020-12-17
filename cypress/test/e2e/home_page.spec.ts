8829af4dec82dc5a9bfa6a5bb59c9043e2af81e8
christophe-chausseray <chausseray.christophe@gmail.com>
2020-12-17 11:37:08 +0100

    Update e2e tests to assert markers on the map

--- cypress/test/e2e/home_page.spec.ts
+++ cypress/test/e2e/home_page.spec.ts
@@ -5,9 +5,13 @@ context('Home page', () => {
     cy.visit('/');
   })
 
-  it('display the title and the map on the homepage', () => {
+  it('display the title and the marker on the map', () => {
       cy.findByRole('heading', {name: /Le bon camping/i}).should('exist');
 
-      cy.findByRole('img', {name: /Map/i}).should('exist').debug();
+      cy.findByRole('img', {name: /Map/i}).should('exist');
+
+      cy.findAllByTestId('camping-marker').should(campingMarkers => {
+        expect(campingMarkers.length).to.equal(2);
+      });
   });
 });
